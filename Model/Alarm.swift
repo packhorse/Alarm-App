6af7fe941217903c13dacf7e139e47d4d9533089
Porter Frazier <porterfrazier@Porters-MacBook-Pro.local>
2018-09-04 15:01:10 -0600

    Initial commit

---
+++ Model/Alarm.swift
@@ -0,0 +1,14 @@
+//
+//  Alarm.swift
+//  AlarmAssignment
+//
+//  Created by Porter Frazier on 9/3/18.
+//  Copyright © 2018 BULB. All rights reserved.
+//
+
+import Foundation
+
+
+class Alarm {
+    
+}
